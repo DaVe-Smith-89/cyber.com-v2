0915e6822756da7d26cfdfba0967aae5f21084b4
Theekshana Pramod <72883295+PRAMOD-max-ux@users.noreply.github.com>
2022-01-17 15:26:18 +0530

    Update settings.py

--- slcyberwarriors/settings.py
+++ slcyberwarriors/settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     BASE_DIR / 'frontend/build/static',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / 'frontend/build/static',
+]
 
-STATIC_ROOT = BASE_DIR / 'frontend/build/static'
+# STATIC_ROOT = BASE_DIR / 'frontend/build/static'
 
 STATIC_URL = 'static/'
 
